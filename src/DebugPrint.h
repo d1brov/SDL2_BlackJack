77cc38ddbd53852304007cb6810675b9ed9f890f
Andrii Dibrov <andrii.dibrov@gmail.com>
2023-08-05 19:06:57 +0300

    Fix debug printing macro

--- src/DebugPrint.h
+++ src/DebugPrint.h
@@ -1,18 +1,17 @@
 #pragma once
 
-#ifndef NDBG
-
-	#define TESTING
-
-	#ifdef TESTING
-		#include <iostream>
-		#include <format>
-		#include <string>
-		#define DBG_FMT_PRINT(msg, args) std::cout << std::format(msg, ##args)
-		#define DBG_PRINT(msg) std::cout << msg
-	#endif
+#ifndef NDEBUG
+	#include <iostream>
+	#include <format>
+	#include <string>
+	#define DBG_FMT_PRINT(msg, args) std::cout << std::format(msg, ##args)
+	#define DBG_PRINT(msg) std::cout << msg
 
 	extern int surface_counter;
 	extern int texture_counter;
+#else
+
+	#define DBG_FMT_PRINT(msg, args);
+	#define DBG_PRINT(msg);
 
 #endif
